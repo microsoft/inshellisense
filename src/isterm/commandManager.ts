7a98e46016c55770b3d63da79d96f4383328d032
Chapman Pendery <cpendery@vt.edu>
2023-11-18 00:04:19 -0800

    refactor: use es private field syntax
    Signed-off-by: Chapman Pendery <cpendery@vt.edu>

--- src/isterm/commandManager.ts
+++ src/isterm/commandManager.ts
@@ -18,18 +18,19 @@ type TerminalCommand = {
 };
 
 export class CommandManager {
-  private _activeCommand: TerminalCommand;
-  private _previousCommandLines: Set<number>;
-  private readonly _supportsProperOscPlacements = os.platform() !== "win32";
+  #activeCommand: TerminalCommand;
+  #previousCommandLines: Set<number>;
+  #terminal: Terminal;
+  #shell: Shell;
+  readonly #supportsProperOscPlacements = os.platform() !== "win32";
 
-  constructor(
-    private readonly _terminal: Terminal,
-    private readonly _shell: Shell,
-  ) {
-    this._activeCommand = {};
-    this._previousCommandLines = new Set();
-    if (this._supportsProperOscPlacements) {
-      this._terminal.parser.registerCsiHandler({ final: "J" }, (params) => {
+  constructor(terminal: Terminal, shell: Shell) {
+    this.#terminal = terminal;
+    this.#shell = shell;
+    this.#activeCommand = {};
+    this.#previousCommandLines = new Set();
+    if (this.#supportsProperOscPlacements) {
+      this.#terminal.parser.registerCsiHandler({ final: "J" }, (params) => {
         if (params.at(0) == 3 || params.at(0) == 2) {
           this.handleClear();
         }
@@ -38,27 +39,27 @@ export class CommandManager {
     }
   }
   handlePromptStart() {
-    this._activeCommand = { promptStartMarker: this._terminal.registerMarker(0), hasOutput: false, cursorTerminated: false };
+    this.#activeCommand = { promptStartMarker: this.#terminal.registerMarker(0), hasOutput: false, cursorTerminated: false };
   }
 
   handlePromptEnd() {
-    this._activeCommand.promptEndMarker = this._terminal.registerMarker(0);
-    if (this._activeCommand.promptEndMarker?.line === this._terminal.buffer.active.cursorY) {
-      this._activeCommand.promptEndX = this._terminal.buffer.active.cursorX;
+    this.#activeCommand.promptEndMarker = this.#terminal.registerMarker(0);
+    if (this.#activeCommand.promptEndMarker?.line === this.#terminal.buffer.active.cursorY) {
+      this.#activeCommand.promptEndX = this.#terminal.buffer.active.cursorX;
     }
-    if (this._supportsProperOscPlacements) {
-      this._activeCommand.promptText = this._terminal.buffer.active.getLine(this._activeCommand.promptEndMarker?.line ?? 0)?.translateToString(true);
-      this._previousCommandLines.add(this._activeCommand.promptEndMarker?.line ?? -1);
+    if (this.#supportsProperOscPlacements) {
+      this.#activeCommand.promptText = this.#terminal.buffer.active.getLine(this.#activeCommand.promptEndMarker?.line ?? 0)?.translateToString(true);
+      this.#previousCommandLines.add(this.#activeCommand.promptEndMarker?.line ?? -1);
     }
   }
 
   handleClear() {
     this.handlePromptStart();
-    this._previousCommandLines = new Set();
+    this.#previousCommandLines = new Set();
   }
 
   private _getWindowsPrompt(y: number) {
-    const line = this._terminal.buffer.active.getLine(y);
+    const line = this.#terminal.buffer.active.getLine(y);
     if (!line) {
       return;
     }
@@ -70,7 +71,7 @@ export class CommandManager {
     // User defined prompt
     // TOOD: allow users to define their own prompt patterns per shell
 
-    if (this._shell == Shell.Bash) {
+    if (this.#shell == Shell.Bash) {
       const gitBashPrompt = lineText.match(/^(?<prompt>\$\s?)/)?.groups?.prompt;
       if (gitBashPrompt) {
         const adjustedPrompt = this._adjustPrompt(gitBashPrompt, lineText, "$");
@@ -88,7 +89,7 @@ export class CommandManager {
       }
     }
 
-    if (this._shell == Shell.Powershell || this._shell == Shell.Pwsh) {
+    if (this.#shell == Shell.Powershell || this.#shell == Shell.Pwsh) {
       const pwshPrompt = lineText.match(/(?<prompt>(\(.+\)\s)?(?:PS.+>\s?))/)?.groups?.prompt;
       if (pwshPrompt) {
         const adjustedPrompt = this._adjustPrompt(pwshPrompt, lineText, ">");
@@ -98,7 +99,7 @@ export class CommandManager {
       }
     }
 
-    if (this._shell == Shell.Cmd) {
+    if (this.#shell == Shell.Cmd) {
       return lineText.match(/^(?<prompt>(\(.+\)\s)?(?:[A-Z]:\\.*>))/)?.groups?.prompt;
     }
 
@@ -124,41 +125,41 @@ export class CommandManager {
   }
 
   termSync() {
-    if (this._activeCommand.promptEndMarker == null || this._activeCommand.promptStartMarker == null) {
+    if (this.#activeCommand.promptEndMarker == null || this.#activeCommand.promptStartMarker == null) {
       return;
     }
-    const promptEndMarker = this._activeCommand.promptEndMarker;
-    const promptStartMarker = this._activeCommand.promptStartMarker;
+    const promptEndMarker = this.#activeCommand.promptEndMarker;
+    const promptStartMarker = this.#activeCommand.promptStartMarker;
 
-    const globalCursorPosition = this._terminal.buffer.active.baseY + this._terminal.buffer.active.cursorY;
-    const withinPollDistance = globalCursorPosition < this._activeCommand.promptEndMarker.line + 5;
+    const globalCursorPosition = this.#terminal.buffer.active.baseY + this.#terminal.buffer.active.cursorY;
+    const withinPollDistance = globalCursorPosition < this.#activeCommand.promptEndMarker.line + 5;
 
     if (globalCursorPosition < promptStartMarker.line) {
       this.handleClear();
     }
 
     // if we haven't fond the prompt yet, poll over the next 5 lines searching for it
-    if (this._activeCommand.promptText == null && withinPollDistance) {
+    if (this.#activeCommand.promptText == null && withinPollDistance) {
       for (let i = globalCursorPosition; i < promptEndMarker.line + 5; i++) {
-        if (this._previousCommandLines.has(i)) continue;
+        if (this.#previousCommandLines.has(i)) continue;
         const promptResult = this._getWindowsPrompt(i);
         if (promptResult != null) {
-          this._activeCommand.promptEndMarker = this._terminal.registerMarker(i - globalCursorPosition);
-          this._activeCommand.promptEndX = promptResult.length;
-          this._activeCommand.promptText = promptResult;
-          this._previousCommandLines.add(i);
+          this.#activeCommand.promptEndMarker = this.#terminal.registerMarker(i - globalCursorPosition);
+          this.#activeCommand.promptEndX = promptResult.length;
+          this.#activeCommand.promptText = promptResult;
+          this.#previousCommandLines.add(i);
         }
       }
     }
 
     // if the prompt is set, now parse out the values from the terminal
-    if (this._activeCommand.promptText != null) {
+    if (this.#activeCommand.promptText != null) {
       let lineY = promptEndMarker.line;
-      let line = this._terminal.buffer.active.getLine(promptEndMarker.line);
+      let line = this.#terminal.buffer.active.getLine(promptEndMarker.line);
       let command = "";
       let suggestions = "";
       for (;;) {
-        for (let i = lineY == promptEndMarker.line ? this._activeCommand.promptText.length : 0; i < this._terminal.cols; i++) {
+        for (let i = lineY == promptEndMarker.line ? this.#activeCommand.promptText.length : 0; i < this.#terminal.cols; i++) {
           const cell = line?.getCell(i);
           if (cell == null) continue;
           if (cell.getFgColor() != 238 && suggestions.length == 0) {
@@ -168,16 +169,16 @@ export class CommandManager {
           }
         }
         lineY += 1;
-        line = this._terminal.buffer.active.getLine(lineY);
+        line = this.#terminal.buffer.active.getLine(lineY);
         if (!line?.isWrapped) {
           break;
         }
       }
 
-      const cursorAtEndOfInput = (this._activeCommand.promptText.length + command.trim().length) % this._terminal.cols <= this._terminal.buffer.active.cursorX;
+      const cursorAtEndOfInput = (this.#activeCommand.promptText.length + command.trim().length) % this.#terminal.cols <= this.#terminal.buffer.active.cursorX;
       let hasOutput = false;
 
-      for (let i = 0; i < this._terminal.cols; i++) {
+      for (let i = 0; i < this.#terminal.cols; i++) {
         const cell = line?.getCell(i);
         if (cell == null) continue;
         hasOutput = cell.getChars() != "";
@@ -186,18 +187,18 @@ export class CommandManager {
         }
       }
 
-      this._activeCommand.hasOutput = hasOutput;
-      this._activeCommand.suggestionsText = suggestions.trim();
-      this._activeCommand.commandText = command.trim();
-      this._activeCommand.cursorTerminated = cursorAtEndOfInput;
+      this.#activeCommand.hasOutput = hasOutput;
+      this.#activeCommand.suggestionsText = suggestions.trim();
+      this.#activeCommand.commandText = command.trim();
+      this.#activeCommand.cursorTerminated = cursorAtEndOfInput;
     }
 
     log.debug(
       JSON.stringify({
         msg: "cmd manager state",
-        ...this._activeCommand,
-        promptEndMarker: this._activeCommand.promptEndMarker?.line,
-        promptStartMarker: this._activeCommand.promptStartMarker?.line,
+        ...this.#activeCommand,
+        promptEndMarker: this.#activeCommand.promptEndMarker?.line,
+        promptStartMarker: this.#activeCommand.promptStartMarker?.line,
       }),
     );
   }
