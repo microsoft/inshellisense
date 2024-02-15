8ced0644d136fddeac894aae5af6057223e11b40
Chapman Pendery <cpendery@vt.edu>
2024-02-15 10:37:05 -0800

    fix: drop hidden option header
    Signed-off-by: Chapman Pendery <cpendery@vt.edu>

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ program
   .action(action(program))
   .option("-s, --shell <shell>", `shell to use for command execution, supported shells: ${supportedShells}`)
   .option("-c, --check", `check if shell is in an inshellisense session`)
-  .addOption(hiddenOption("-T, --test", "*private* used to make e2e tests reproducible across machines"))
+  .addOption(hiddenOption("-T, --test", "used to make e2e tests reproducible across machines"))
   .option("-V, --verbose", `enable verbose logging`)
   .showHelpAfterError("(add --help for additional information)");
 
