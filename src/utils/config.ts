85d00ce10b8e94ac963c14d0aaa96023a430409e
Chapman Pendery <cpendery@vt.edu>
2024-03-09 13:08:03 -0800

    feat: add keybinding support
    Signed-off-by: Chapman Pendery <cpendery@vt.edu>

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,6 +5,7 @@ import os from "node:os";
 import path from "node:path";
 import fs from "node:fs";
 import fsAsync from "node:fs/promises";
+import toml from "toml";
 import _Ajv, { JSONSchemaType } from "ajv";
 import { Command } from "commander";
 
@@ -15,72 +16,126 @@ type PromptPattern = {
   regex: string;
   postfix: string;
 };
+
+type Binding = {
+  shift?: boolean;
+  control?: boolean;
+  key: string;
+};
+
 type Config = {
-  promptRegex?: {
-    bash?: PromptPattern;
-    pwsh?: PromptPattern;
-    powershell?: PromptPattern;
+  bindings: {
+    nextSuggestion: Binding;
+    previousSuggestion: Binding;
+    dismissSuggestions: Binding;
+    acceptSuggestion: Binding;
+  };
+  prompt?: {
+    bash?: PromptPattern[];
+    pwsh?: PromptPattern[];
+    xonsh?: PromptPattern[];
+    powershell?: PromptPattern[];
   };
 };
 
-const configSchema: JSONSchemaType<Config> = {
+const bindingSchema: JSONSchemaType<Binding> = {
   type: "object",
+  nullable: true,
   properties: {
-    promptRegex: {
+    shift: { type: "boolean", nullable: true },
+    control: { type: "boolean", nullable: true },
+    key: { type: "string" },
+  },
+  required: ["key"],
+};
+
+const promptPatternsSchema: JSONSchemaType<PromptPattern[]> = {
+  type: "array",
+  nullable: true,
+  items: {
+    type: "object",
+    properties: {
+      regex: { type: "string" },
+      postfix: { type: "string" },
+    },
+    required: ["regex", "postfix"],
+  },
+};
+
+const configSchema = {
+  type: "object",
+  nullable: true,
+  properties: {
+    bindings: {
       type: "object",
-      properties: {
-        bash: {
-          type: "object",
-          nullable: true,
-          properties: {
-            regex: { type: "string" },
-            postfix: { type: "string" },
-          },
-          required: ["regex", "postfix"],
-        },
-        pwsh: {
-          type: "object",
-          nullable: true,
-          properties: {
-            regex: { type: "string" },
-            postfix: { type: "string" },
-          },
-          required: ["regex", "postfix"],
-        },
-        powershell: {
-          type: "object",
-          nullable: true,
-          properties: {
-            regex: { type: "string" },
-            postfix: { type: "string" },
-          },
-          required: ["regex", "postfix"],
-        },
-      },
       nullable: true,
+      properties: {
+        nextSuggestion: bindingSchema,
+        previousSuggestion: bindingSchema,
+        dismissSuggestions: bindingSchema,
+        acceptSuggestion: bindingSchema,
+      },
+    },
+    prompt: {
+      type: "object",
+      nullable: true,
+      properties: {
+        bash: promptPatternsSchema,
+        pwsh: promptPatternsSchema,
+        powershell: promptPatternsSchema,
+        xonsh: promptPatternsSchema,
+      },
     },
   },
   additionalProperties: false,
 };
 
-const configFolder = ".inshellisense";
-const cachePath = path.join(os.homedir(), configFolder, "config.json");
+const configFile = ".inshellisenserc";
+const cachePath = path.join(os.homedir(), ".inshellisense");
+const configPath = path.join(os.homedir(), configFile);
 
-let globalConfig: Config = {};
+let globalConfig: Config;
 export const getConfig = (): Config => globalConfig;
 export const loadConfig = async (program: Command) => {
-  if (fs.existsSync(cachePath)) {
-    const config = JSON.parse((await fsAsync.readFile(cachePath)).toString());
+  if (fs.existsSync(configPath)) {
+    let config: Config;
+    try {
+      config = toml.parse((await fsAsync.readFile(configPath)).toString());
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (e: any) {
+      program.error(`${configFile} is invalid toml. Parsing error on line ${e.line}, column ${e.column}: ${e.message}`);
+    }
     const isValid = ajv.validate(configSchema, config);
     if (!isValid) {
-      program.error("inshellisense config is invalid: " + ajv.errorsText());
+      program.error(`${configFile} is invalid: ${ajv.errorsText()}`);
     }
-    globalConfig = config;
+    globalConfig = {
+      bindings: {
+        nextSuggestion: config?.bindings?.nextSuggestion ?? {
+          key: "down",
+        },
+        previousSuggestion: config?.bindings?.previousSuggestion ?? {
+          key: "up",
+        },
+        acceptSuggestion: config?.bindings?.acceptSuggestion ?? {
+          key: "tab",
+        },
+        dismissSuggestions: config?.bindings?.dismissSuggestions ?? {
+          key: "escape",
+        },
+      },
+      prompt: {
+        bash: config.prompt?.bash,
+        powershell: config.prompt?.powershell,
+        xonsh: config.prompt?.xonsh,
+        pwsh: config.prompt?.pwsh,
+      },
+    };
   }
 };
 
-export const deleteConfigFolder = async (): Promise<void> => {
-  const cliConfigPath = path.join(os.homedir(), configFolder);
+export const deleteCacheFolder = async (): Promise<void> => {
+  const cliConfigPath = path.join(os.homedir(), cachePath);
   if (fs.existsSync(cliConfigPath)) {
     fs.rmSync(cliConfigPath, { recursive: true });
   }
