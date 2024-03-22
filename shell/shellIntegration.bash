710f256da3a3eed521747af6789bf7df3fd3d480
Russell Davis <551404+russelldavis@users.noreply.github.com>
2024-03-22 09:14:30 -0700

    fix: don't source bash startup files meant for login shells (#214)
    Fixes #213

--- shell/shellIntegration.bash
+++ shell/shellIntegration.bash
@@ -1,16 +1,6 @@
 if [ -r ~/.bashrc ]; then
     . ~/.bashrc
 fi
-if [ -r /etc/profile ]; then
-    . /etc/profile
-fi
-if [ -r ~/.bash_profile ]; then
-    . ~/.bash_profile
-elif [ -r ~/.bash_login ]; then
-    . ~/.bash_login
-elif [ -r ~/.profile ]; then
-    . ~/.profile
-fi
 
 if [ -r ~/.inshellisense/bash-preexec.sh ]; then
     . ~/.inshellisense/bash-preexec.sh
