5dffc675a27a5b1b13f67cc0a64feb0f5edcc3da
Chapman Pendery <35637443+cpendery@users.noreply.github.com>
2024-08-26 13:14:50 -0700

    fix: zsh configs on macos (#275)
    Signed-off-by: Chapman Pendery <cpendery@vt.edu>

--- shell/shellIntegration-env.zsh
+++ shell/shellIntegration-env.zsh
@@ -1,9 +1,12 @@
 if [[ -f $USER_ZDOTDIR/.zshenv ]]; then
+	IS_ZDOTDIR=$ZDOTDIR
 	ZDOTDIR=$USER_ZDOTDIR
 
 	# prevent recursion
-	if [[ $USER_ZDOTDIR != $ZDOTDIR ]]; then
-		ZDOTDIR=$USER_ZDOTDIR
+	if [[ $USER_ZDOTDIR != $IS_ZDOTDIR ]]; then
 		. $USER_ZDOTDIR/.zshenv
 	fi
+
+	USER_ZDOTDIR=$ZDOTDIR
+	ZDOTDIR=$IS_ZDOTDIR
 fi
