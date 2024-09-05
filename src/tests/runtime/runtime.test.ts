e296b679da2991f74d5f94bcf28dcb1b864f4cbc
Chapman Pendery <cpendery@vt.edu>
2024-09-05 11:21:07 -0700

    feat: add tests and bump @withfig/autocomplete to 2.675.0
    Signed-off-by: Chapman Pendery <cpendery@vt.edu>

--- src/tests/runtime/runtime.test.ts
+++ src/tests/runtime/runtime.test.ts
@@ -1,8 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import fs from "node:fs";
+
 import { getSuggestions } from "../../runtime/runtime.js";
 import { Shell } from "../../utils/shell.js";
+import { SuggestionIcons } from "../../runtime/suggestion.js";
 
 const testData = [
   { name: "partialPrefixFilter", command: "git sta" },
@@ -39,20 +42,48 @@ describe(`parseCommand`, () => {
   });
 });
 
-const platformTestData = [
-  { name: "macosSpawnCommand", command: "brew install mullvad-brow", platform: "darwin", maxSuggestions: 1, expectedNames: ["mullvad-browser"] },
+const commandSuggestionsData = [
+  {
+    name: "brewInstallMullvad",
+    command: "brew install mullvad-brow",
+    platforms: ["darwin"],
+    maxSuggestions: 1,
+    expectedNames: ["mullvad-browser"],
+    expectedIcons: ["🍺"],
+  },
+  { name: "gitStat", command: "git stat", maxSuggestions: 1, expectedNames: ["status"], expectedIcons: [SuggestionIcons.Subcommand] }, // subcommand generator
+  { name: "gitStatus", command: "git status ", expectedNames: ["demo.ts"] }, // script + post-process generator
+  { name: "ls", command: "ls ", maxSuggestions: 1, expectedNames: [".eslintrc.cjs"], expectedIcons: [SuggestionIcons.File] }, // file generator
+  { name: "cd", command: "cd ", maxSuggestions: 1, expectedNames: ["build/"], expectedIcons: [SuggestionIcons.Folder] }, // folder generator
+  { name: "find", command: "find -", maxSuggestions: 1, expectedIcons: [SuggestionIcons.Option] }, // filtering file generator
+  { name: "goTool", command: "go build -buildmode ", maxSuggestions: 1, expectedNames: ["archive"] }, // script + split-on generator
+  { name: "preCommitRun", command: "pre-commit run ", maxSuggestions: 1, expectedIcons: [SuggestionIcons.Option] }, // script + post-process generator w/ console logs
 ];
 
-describe(`parsePlatformCommand`, () => {
-  platformTestData.forEach(({ command, name, maxSuggestions, expectedNames, platform }) => {
-    if (platform != null && process.platform != platform) return;
+describe(`getCommandSuggestions`, () => {
+  beforeAll(async () => await getCommandSuggestionsSetup());
+
+  commandSuggestionsData.forEach(({ command, name, maxSuggestions, expectedNames, expectedIcons, platforms }) => {
+    if (platforms != null && !platforms.includes(process.platform)) return;
     test(name, async () => {
       const suggestions = await getSuggestions(command, process.cwd(), Shell.Cmd);
       if (suggestions != null && suggestions.suggestions != null) {
         suggestions.suggestions = suggestions?.suggestions.slice(0, maxSuggestions);
       }
       const names = suggestions?.suggestions.map((s) => s.allNames).flat() ?? [];
-      expect(names).toEqual(expect.arrayContaining(expectedNames));
+      const icons = suggestions?.suggestions.map((s) => s.icon) ?? [];
+      expect(names).toEqual(expect.arrayContaining(expectedNames ?? []));
+      expect(icons).toEqual(expect.arrayContaining(expectedIcons ?? []));
     });
   });
+
+  afterAll(async () => await getCommandSuggestionsCleanup());
 });
+
+const getCommandSuggestionsSetup = async () => {
+  fs.closeSync(fs.openSync("demo.ts", "w"));
+};
+
+const getCommandSuggestionsCleanup = async () => {
+  fs.rmSync("demo.ts");
+};
