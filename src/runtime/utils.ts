e021a9be0fd27156329cfb9ca09dc5b34880e8e8
Chapman Pendery <35637443+cpendery@users.noreply.github.com>
2024-10-18 12:56:38 -0400

    fix: use git bash for shell commands on windows to reduce generator failures (#290)
    * fix: use git bash for shell commands on windows to reduce generator failures
    
    Signed-off-by: Chapman Pendery <cpendery@vt.edu>
    
    * fix: add quoting to git bash arguments when containing special characters on windows
    
    Signed-off-by: Chapman Pendery <cpendery@vt.edu>
    
    ---------
    
    Signed-off-by: Chapman Pendery <cpendery@vt.edu>

--- src/runtime/utils.ts
+++ src/runtime/utils.ts
@@ -6,17 +6,44 @@ import { spawn } from "node:child_process";
 import fsAsync from "node:fs/promises";
 
 import { CommandToken } from "./parser.js";
-import { getPathSeperator, Shell } from "../utils/shell.js";
+import { getPathSeperator, gitBashPath, Shell } from "../utils/shell.js";
 import log from "../utils/log.js";
 
 export type ExecuteShellCommandTTYResult = {
   code: number | null;
 };
 
+const getExecutionShell = async (): Promise<undefined | string> => {
+  if (process.platform !== "win32") return;
+  try {
+    return await gitBashPath();
+  } catch (e) {
+    log.debug({ msg: "failed to load posix shell for windows child_process.spawn, some generators might fail", error: e });
+  }
+};
+
+const bashSpecialCharacters = /[&|<>\s]/g;
+// escape whitespace & special characters in an argument when not quoted
+const shouldEscapeArg = (arg: string) => {
+  const hasSpecialCharacter = bashSpecialCharacters.test(arg);
+  const isSingleCharacter = arg.length === 1;
+  const isQuoted = (arg.startsWith(`"`) && arg.endsWith(`"`)) || (arg.startsWith(`'`) && arg.endsWith(`'`));
+  return hasSpecialCharacter && !isSingleCharacter && !isQuoted;
+};
+
+/* based on libuv process.c used by nodejs, only quotes are escaped for shells. if using git bash need to escape whitespace & special characters in an argument */
+const escapeArgs = (shell: string | undefined, args: string[]) => {
+  // only escape args for git bash
+  if (process.platform !== "win32" || shell == undefined) return args;
+  return args.map((arg) => (shouldEscapeArg(arg) ? `"${arg.replaceAll('"', '\\"')}"` : arg));
+};
+
 export const buildExecuteShellCommand =
-  (timeout: number): Fig.ExecuteCommandFunction =>
+  async (timeout: number): Promise<Fig.ExecuteCommandFunction> =>
   async ({ command, env, args, cwd }: Fig.ExecuteCommandInput): Promise<Fig.ExecuteCommandOutput> => {
-    const child = spawn(command, args, { cwd, env: { ...process.env, ...env, ISTERM: "1" } });
+    const executionShell = await getExecutionShell();
+    const escapedArgs = escapeArgs(executionShell, args);
+    const child = spawn(command, escapedArgs, { cwd, env: { ...process.env, ...env, ISTERM: "1" }, shell: executionShell });
     setTimeout(() => child.kill("SIGKILL"), timeout);
     let stdout = "";
     let stderr = "";
