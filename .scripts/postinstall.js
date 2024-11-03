348e43fea3e9b340a0490c063dd5e3dad83fee5a
Chapman Pendery <cpendery@vt.edu>
2024-11-03 00:56:09 -0700

    fix: use js postinstall hook
    Signed-off-by: Chapman Pendery <cpendery@vt.edu>

---
+++ .scripts/postinstall.js
@@ -0,0 +1,6 @@
+import fs from "node:fs";
+
+if (fs.existsSync("./build/commands/init.js")) {
+  const init = (await import("../build/commands/init.js")).default;
+  init.parse(["--generate-full-configs"], { from: "user" });
+}
